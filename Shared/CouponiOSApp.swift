5bca0b1e39297b8b6e8570a6be8c5ce15b048b09
joe061005 <18233368@life.hkbu.edu.hk>
2021-10-07 22:09:35 +0800

    Initial Commit

---
+++ Shared/CouponiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouponiOSApp.swift
+//  Shared
+//
+//  Created by joe  on 7/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CouponiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
